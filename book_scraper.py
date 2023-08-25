82b8addd234ef8e15e273fa2af4f685e10f7ba7e
Dandara <dandaraleite2@gmail.com>
2023-08-24 22:24:36 -0300

    create book scraper by category

---
+++ book_scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class Book:
+    def __init__(self, title, review, price, image_url):
+        self.title = title
+        self.review = review
+        self.price = price
+        self.image_url = image_url
+
+class BookScraper:
+    BASE_URL = "https://books.toscrape.com"
+
+    def __init__(self):
+        self.books = []
